b646b0d6c104e7789bfbda0ae0aba34aef324673
Андрей <pylovvandrej@gmail.com>
2025-10-02 20:33:11 +0700

    Update homework_01.10.25_AA.py
    This very good code!

--- Informatics/homework_01.10.25_AA.py
+++ Informatics/homework_01.10.25_AA.py
@@ -1,9 +1,3 @@
-code = f''' ''' # необработанный код
-inp = 160 # который заменять
-out = ' ' # которым заменить
-for j in code:
-    if ord(j) == inp:
-        code = code.replace(j, out)
-    else:
-        pass
-print(code) 
+print('Script:')
+data = input('>>> ')
+print(data.replace(chr(160), ' ', data.count(chr(160))))
