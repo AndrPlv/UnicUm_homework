abd21c10b44981c846a91fba0a52308a1ab1dffa
Андрей <pylovvandrej@gmail.com>
2026-01-23 00:10:17 +0700

    Update AutoWater.ino

--- ArduinoProject/AutoWater.ino
+++ ArduinoProject/AutoWater.ino
@@ -1,15 +1,14 @@
+#include <iarduino_DHT.h>
 #include <LiquidCrystal_I2C.h> // Библиотека для LCD
-#include "DHT.h"
 
 
 #define POMP 7 // пин для помпы
-#define SIGPOMP 8 // сигнальный пин для помпы
-#define SYS 9 // сигнальнальный пин 
+#define SYS 13 // сигнальнальный пин 
 #define DHTPIN 2     // Пин, к которому подключен датчик
-#define DHTTYPE DHT22   // Тип датчика DHT 22 (AM2302)
+#define SIG_POMP 12
 
-DHT dht(DHTPIN, DHTTYPE);
-// Создаем объект: адрес, столбцы, строки
+
+iarduino_DHT sensor(DHTPIN);
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Адрес 0x27, дисплей 16x2
 bool water = 0;
 
@@ -56,45 +55,42 @@ void setup() {
   lcd.backlight();   // Включение подсветки
   up(0,0,0,0);
   pinMode(POMP, OUTPUT); // Пин для помпы
-  pinMode(SIGPOMP, OUTPUT); // Пин для помпы  
+  pinMode(SIG_POMP, OUTPUT); // Пин для помпы  
   pinMode(SYS, OUTPUT); // Пин для помпы  
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  digitalWrite(POMP, HIGH);
-  dht.begin();
 }
 
 void loop() {
-    digitalWrite(SYS, HIGH);
-    int t_need = map(analogRead(A0), 0, 1023, 0, 80);
-    int h_need = map(analogRead(A1), 0, 1023, 0, 100);
-    int h = dht.readHumidity();
-    int t = dht.readTemperature();
+    if (sensor.read() == DHT_OK) {
+         int t_need = map(analogRead(A0), 0, 1023, 0, 80);
+         int h_need = map(analogRead(A0), 0, 1023, 0, 10);
 
-    up(t,h,t_need,h_need);  
+         int t = sensor.tem;
+         int h = sensor.hum;
 
+         
+         up(t,h,t_need,h_need);
 
-    if (t > t_need or h < h_need) {
-      water = 1;
+         if ((t > t_need) or (h < h_need)) {
+              water = true;
+         }   
+         else {
+          water = false;
+         }
+    
+    
+        if (water) {
+          digitalWrite(SIG_POMP, HIGH);         
+          digitalWrite(POMP, HIGH);     
+        }
+        if (not(water)) {
+          digitalWrite(SIG_POMP, LOW);
+          digitalWrite(POMP, LOW);
+          digitalWrite(SYS, HIGH);
+          delay(500);
+          digitalWrite(SYS, LOW);          
+          }
+        delay(500);
     }
-    else {
-      water = 0;
-    }
-
-
-
-    if (water) {
-      digitalWrite(POMP, HIGH);
-      digitalWrite(SIGPOMP, HIGH);
-
-    }
-    else {
-      digitalWrite(SIGPOMP, LOW);
-      digitalWrite(POMP, LOW);  
-    }
-    delay(1000);
-    digitalWrite(SYS, LOW);
-    delay(500);
-    digitalWrite(SYS, HIGH);
-    delay(500);
 }
